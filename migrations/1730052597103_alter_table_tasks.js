422ddd9fda2c7300f01e9e66858191695fd6263e
afarghaly10 <sami.farghaly@gmail.com>
2024-10-27 17:30:36 -0400

    schema update

--- migrations/1730052597103_alter_table_tasks.js
+++ migrations/1730052597103_alter_table_tasks.js
@@ -1,7 +1,7 @@
 module.exports = {
 	up:
 		'ALTER TABLE `Tasks` ' +
-		'MODIFY `status` enum(\'To Do\',\'In Progress\',\'Ready For Review\',\'Completed\') DEFAULT \'To Do\', ' +
+		'MODIFY `status` enum(\'To Do\',\'In Progress\',\'Under Review\',\'Completed\') DEFAULT \'To Do\', ' +
 		'MODIFY `priority` enum(\'Urgent\',\'High\',\'Medium\',\'Low\',\'Backlog\') DEFAULT \'Backlog\'; ',
 	down: '',
 };
